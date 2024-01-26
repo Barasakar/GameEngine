4dde5e9ed18c2bebc2bb75b9fa03c9d05172cef7
Jonathan <jlinpoughkeepsieday@gmail.com>
2024-01-25 23:19:21 -0600

    rendered a window

--- GameEngine/GameEngine.cpp
+++ GameEngine/GameEngine.cpp
@@ -2,9 +2,37 @@
 //
 
 #include <iostream>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
 
+using namespace std;
 int main()
 {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Game Engine", NULL, NULL);
+    if (!window) {
+
+        cout << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
     std::cout << "Hello World!\n";
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        cout << "Failed to initialize GLAD" << endl;
+        return -1;
+    }
+    glViewport(0, 0, 800, 600);
+
+    // A render loop so the window wont be closed immediately.
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 }
 
