a972a3298f136e5adfdec80908a0d52d854fb22a
Jonathan <jlinpoughkeepsieday@gmail.com>
2024-02-03 00:18:14 -0600

    finished learning how to write vertex shader

--- GameEngine/GameEngine.cpp
+++ GameEngine/GameEngine.cpp
@@ -17,12 +17,57 @@ void processInput(GLFWwindow* window) {
     }
 }
 
+// Vertex Shader: Stage one of graphics pipline
+void vertexShader() {
+    int success;
+    char infoLog[512];
+    // Triangle
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f
+    };
+
+    // Shader Language GLSL
+    const char* vertexShaderSource = "#version 330 core\n" 
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+
+    unsigned int vertexShader;
+    // Allocate memory on GPU and assign a bufferID to the variable VBO.
+    unsigned int VBO;
+    
+    glGenBuffers(1, &VBO);
+
+    // Bind the newly create Buffer to GL_ARRAY_BUFFER target.
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::VERTEXSHADER::COMPILATION::FAILED\n" << infoLog << endl;
+    }
+}
+
+void fragmentShader() {
+
+}
 int main()
 {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "Game Engine", NULL, NULL);
     if (!window) {
@@ -40,6 +85,7 @@ int main()
         return -1;
     }
   
+    vertexShader();
 
     // A render loop so the window wont be closed immediately.
     while (!glfwWindowShouldClose(window)) {
